2c76c89132cc16657cbb839d68bf9f3eaab6c032
EmrD <emrd434@gmail.com>
2025-01-31 19:46:44 +0300

    File edited

--- demo/src/main/java/com/example/demo/BaseContructors/UserRepository.java
+++ demo/src/main/java/com/example/demo/BaseContructors/UserRepository.java
@@ -3,4 +3,4 @@ package com.example.demo.BaseContructors;
 import com.example.demo.Models.testtable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<testtable, String> {} //
+public interface UserRepository extends JpaRepository<testtable, String> {} // testtable is our table name
